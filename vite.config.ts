80cba664827913d615c10640b4985656705b0349
Dennis McCarthy <dmccuk@users.noreply.github.com>
2025-12-29 09:17:27 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
+//import { defineConfig } from "vite";
+//
+//const repoName = "tbs_elite"; // Change this to match your GitHub repo name
+//export default defineConfig({
+//  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+//});
 import { defineConfig } from "vite";
 
-const repoName = "tbs_elite"; // Change this to match your GitHub repo name
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+  base: '/',
 });
